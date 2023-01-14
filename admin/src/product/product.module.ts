8e610c52ffd9a8ecc973cdc5b0153abb7c5cf1b4
wardvisual <wardvisual@gmail.com>
2023-01-14 23:43:09 +0000

    chore: hide credentails

--- admin/src/product/product.module.ts
+++ admin/src/product/product.module.ts
@@ -5,18 +5,18 @@ import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forFeature([Product]),
     ClientsModule.register([
       {
         name: 'USER_SERVICE',
         transport: Transport.RMQ,
         options: {
-          urls: [
-            'amqps://mxmhvdbn:Z0LqyMp8dVRflJS_AbYJ9JLOmiLEXg-z@stingray.rmq.cloudamqp.com/mxmhvdbn',
-          ],
+          urls: [process.env.AMQP_URL],
           queue: 'user_queue',
           queueOptions: {
             durable: false,
