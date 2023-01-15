2834271c246079ceac324c8b78cd2f6b855cf0df
wardvisual <wardvisual@gmail.com>
2023-01-15 00:03:34 +0000

    chore: subscribe to admin service events

--- user/src/product/product.controller.ts
+++ user/src/product/product.controller.ts
@@ -17,9 +17,9 @@ import { EventPattern } from '@nestjs/microservices';
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
-  @Post()
-  async create(@Body() createProductDto: CreateProductDto): Promise<Product> {
-    return await this.productService.create(createProductDto);
+  @EventPattern('product_created')
+  async create(product: Product): Promise<Product> {
+    return await this.productService.create(product);
   }
 
   @Get()
@@ -27,8 +27,10 @@ export class ProductController {
     return await this.productService.find();
   }
 
-  @EventPattern('hello')
-  async hello(data: string) {
-    console.log({ data });
+  @EventPattern('product_updated')
+  async update(product: Product) {
+    const _product = await this.productService.update(product.id, product);
+
+    return _product;
   }
 }
