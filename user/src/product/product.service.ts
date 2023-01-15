ed0f626a6d57e45ddb4f910987836167b90f6152
wardvisual <wardvisual@gmail.com>
2023-01-15 01:17:16 +0000

    chore: perform HTTP Request on admin service

--- user/src/product/product.service.ts
+++ user/src/product/product.service.ts
@@ -4,12 +4,17 @@ import { UpdateProductDto } from './dto/update-product.dto';
 import { Product, ProductDocument } from './entities/product.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { HttpService } from '@nestjs/axios';
+import { Response } from 'express';
 
 @Injectable()
 export class ProductService {
+  private adminURL = `http://localhost:8000/api/product`;
+
   constructor(
     @InjectModel(Product.name)
     private productModel: Model<ProductDocument>,
+    private httpService: HttpService,
   ) {}
 
   async create(createProductDto: CreateProductDto): Promise<Product> {
@@ -22,18 +27,38 @@ export class ProductService {
     return this.productModel.find().exec();
   }
 
-  async update(id: number, updateProductDto: UpdateProductDto) {
-    const product = await this.productModel.findById(id);
+  async findOne(id: number): Promise<Product> {
+    const product = await this.productModel.findOne({ id });
 
     if (!product) {
-      throw new NotFoundException(
-        `Cannot update product with an ID of ${id} as it was not found.`,
-      );
+      throw new NotFoundException(`Product with an ID of ${id} was not found.`);
     }
+
+    return product;
+  }
+
+  async like(id: number) {
+    const product = await this.findOne(id);
+
+    const likeAdded = product.likes + 1;
+
+    this.httpService
+      .patch(`${this.adminURL}/${id}/like`, { likes: likeAdded })
+      .subscribe((res) => {
+        console.log({ res });
+      });
+
+    return await this.updateOne(id, {
+      likes: likeAdded,
+    });
+  }
+
+  async updateOne(id: number, updateProductDto: UpdateProductDto) {
+    const product = await this.findOne(id);
 
     product.updatedAt = new Date(Date.now());
 
-    return await this.productModel.updateOne({ _id: id }, [
+    return await this.productModel.updateOne({ id }, [
       { $set: updateProductDto },
     ]);
   }
